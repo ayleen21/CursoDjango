f37a68f8ee646c1c5c6f2d35d7d3749bd8a8c475
ayleen21 <ayleenmurillo77@gmail.com>
2022-01-27 21:46:19 -0500

    Crear

--- hades-master/app/core/erp/urls.py
+++ hades-master/app/core/erp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
+from core.erp.views import myfirstview,mysecondview
 
-from core.erp.views import myfirstview
+app_name= 'erp'
 
 urlpatterns = [
-    path('uno/',myfirstview),
-    path('dos/',myfirstview)
+    path('uno/',myfirstview,name='vista1'),
+    path('dos/',mysecondview,name='vista2')
 ]
--- hades-master/app/core/erp/views.py
+++ hades-master/app/core/erp/views.py
@@ -1,8 +1,18 @@
-from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
+from core.erp.models import Category, Product
+
 def myfirstview(request): #Creando vista url
-    data = {  #Creacion de diccionario
-        'name': 'Ayleen' 
+    data = {                                 #Creacion de diccionario
+        'name': 'Ayleen' ,
+        'categories': Category.objects.all()
     }
-    return JsonResponse(data)
+    return render(request, 'index.html',data)
+
+def mysecondview(request):
+    data = {                               
+        'name': 'Ayleen' ,
+        'categories': Category.objects.all(),
+        'products': Product.objects.all()
+    }
+    return render(request,'second.html', data)
