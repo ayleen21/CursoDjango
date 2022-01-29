470a47d216b3535bc3d61d5013df1f32231da1b8
ayleen21 <ayleenmurillo77@gmail.com>
2022-01-29 00:41:56 -0500

    Subir avances temas nuevos

--- hades-master/app/core/erp/views.py
+++ hades-master/app/core/erp/views.py
@@ -7,7 +7,7 @@ def myfirstview(request): #Creando vista url
         'name': 'Ayleen' ,
         'categories': Category.objects.all()
     }
-    return render(request, 'index.html',data)
+    return render(request, 'home.html',data)
 
 def mysecondview(request):
     data = {                               
